15555a6cb91cd3c01abf4f4c563a7109040ad934
Felipe de Jesus Ruiz Garcia <felipe.ruiz@washpost.com>
2021-02-15 04:56:15 -0500

    Add symlink to the codebuild script
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe.ruiz@washpost.com>

--- aws/codebuild/setup_codebuild.sh
+++ aws/codebuild/setup_codebuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker pull amazon/aws-codebuild-local:latest --disable-content-trust=false -q &
+docker pull amazon/aws-codebuild-local:latest --disable-content-trust=false -q &> /dev/null &
 
 git clone https://github.com/aws/aws-codebuild-docker-images.git
 cd aws-codebuild-docker-images
 cd ubuntu/standard/4.0
-docker build -t aws/codebuild/standard:4.0 . -q &
+docker build -t aws/codebuild/standard:4.0 . -q &> /dev/null &
 
-# To enter to the standart w
-# docker run -it --entrypoint sh aws/codebuild/standard:4.0 -c bash
-
+# create sym link
+cd ../../../local_builds/
+ln -s `pwd`/codebuild_build.sh ~/codebuild_build.sh
