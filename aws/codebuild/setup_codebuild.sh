a2f5468482e5b53145a153f175e08ad894c7f953
Felipe de Jesus Ruiz Garcia <felipe.ruiz@washpost.com>
2021-02-16 04:25:37 -0500

    Improve the setup_codebuild.sh scritp
    Signed-off-by: Felipe de Jesus Ruiz Garcia <felipe.ruiz@washpost.com>

--- aws/codebuild/setup_codebuild.sh
+++ aws/codebuild/setup_codebuild.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-docker pull amazon/aws-codebuild-local:latest --disable-content-trust=false -q &> /dev/null &
+docker pull amazon/aws-codebuild-local:latest --disable-content-trust=false &
 
 cd ~/
 git clone https://github.com/aws/aws-codebuild-docker-images.git
 cd aws-codebuild-docker-images
 cd ubuntu/standard/4.0
-docker build -t aws/codebuild/standard:4.0 . -q &> /dev/null &
+docker build -t aws/codebuild/standard:4.0 .
 
 # create sym link
 cd ../../../local_builds/
 ln -s `pwd`/codebuild_build.sh ~/codebuild_build.sh
+
+echo "SETUP FINISHED"
